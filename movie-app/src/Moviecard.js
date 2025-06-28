5513d8af38a6394275168dc6913effc4311b6086
bhavyasree2004 <bhavyasreecodes@gmail.com>
2025-06-28 12:10:54 +0530

    updates-in-movie-app

--- movie-app/src/Moviecard.js
+++ movie-app/src/Moviecard.js
@@ -1,7 +1,38 @@
 import { Component } from "react";
 
 export default class MovieCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "The Avengers",
+      plot: "Super natural powers shown in the movie",
+      price: 199,
+      stars: 0,
+    };
+    this.addStars = this.addStars.bind(this);
+    this.removeStars = this.removeStars.bind(this);
+  }
+
+  addStars() {
+    if(this.state.stars>=5){
+      return;
+    }
+    this.setState((prevState) => ({
+      stars: prevState.stars + 0.5,
+    }));
+  }
+
+  removeStars() {
+    if(this.state.stars<=0){
+      return;
+    }
+    this.setState((prevState) => ({
+      stars: prevState.stars - 0.5,
+    }));
+  }
+
   render() {
+    const { title, plot, price, stars } = this.state;
     return (
       <div className="main">
         <div className="movie-card">
@@ -9,22 +40,35 @@ export default class MovieCard extends Component {
             <img src="/Images/avengers.jpg" alt="Movie Poster" />
           </div>
           <div className="right">
-            
-              <div className="title">The Avengers</div>
-              <div className="plot">
-                Super natural power shown in the movie
-              </div>
-              <div className="price">Rs: 199</div>
-            
+            <div className="title">{title}</div>
+            <div className="plot">{plot}</div>
+            <div className="price">Rs: {price}</div>
+
             <div className="footer">
               <div className="rating">8.9</div>
               <div className="star-dis">
-                stars
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/9146/9146915.png"
+                  className="str-btn"
+                  alt="decrease"
+                  onClick={this.removeStars}
+                />
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png"
+                  className="stars"
+                />
+                <img
+                  src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png"
+                  className="str-btn"
+                  alt="increase"
+                  onClick={this.addStars}
+                />
+                <span>{stars}</span>
               </div>
               <div className="button">
-                  <button className="favourite-btn">Favorite</button>
-                  <button className="cart-btn">Add</button>
-                </div>
+                <button className="favourite-btn">Favorite</button>
+                <button className="cart-btn">Add</button>
+              </div>
             </div>
           </div>
         </div>
